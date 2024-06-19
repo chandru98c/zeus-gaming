200fad805ed2a5915c0b7152c040ddc40fa78d1e
chandru98c <santhosh98c@gmail.com>
2024-06-19 10:27:51 +0530

    animation index

--- script/script.js
+++ script/script.js
@@ -10,112 +10,24 @@ close.onclick = () => {
   navlist.classList.remove("open");
 };
 
+
+
 // scroll reveal
 
 const sr = ScrollReveal();
 
 const animation = {
   desktop: {
-    navBar: {
-      origin: "top",
-      distance: "80px",
-      duration: 900,
-   
-      opacity: 0,
-      scale: 0,
-      mobile: false
-    },
-    top: {
-      origin: "top",
-      distance: "60px",
-      duration: 900,
-      reset: true,
-      opacity: 0,
-      scale: 0,
-      mobile: false
-    },
-    popup: {
-      duration: 900,
-      reset: true,
-      opacity: 0,
-      scale: 0,
-      mobile: false
-    },
-    left: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2,
-      mobile: false
-    },
-    right: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2,
-      mobile: false
-    },
-    bottom: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2,
-      origin: "bottom",
-      distance: "80px",
-      mobile: false
-    },
     Home_zoomin: {
-        duration: 900,
-        opacity: 1,
-        scale: 0,
-        origin:"bottom",
-        distance: '100px',
-        mobile: false
-      },
-  
-    zoomout: {
       duration: 900,
-
       opacity: 1,
-      scale: 1.7,
+      scale: 0,
+      origin: "bottom",
+      distance: "100px",
       mobile: false
     }
   },
 
-  mobile: {
-    top: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
-    popup: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
-    left: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
-    right: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
-    bottom: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2,
-      origin: "bottom"
-    },
-  
-  },
   allGadgets: {
     navBar: {
       origin: "top",
@@ -129,28 +41,11 @@ const animation = {
       origin: "top",
       distance: "60px",
       duration: 900,
-      reset: true,
+      reset: false,
       opacity: 0,
       scale: 0
     },
-    popup: {
-      duration: 900,
-      reset: true,
-      opacity: 0,
-      scale: 0
-    },
-    left: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
-    right: {
-      duration: 900,
-      reset: true,
-      opacity: 1,
-      scale: 1.2
-    },
+
     bottom: {
       duration: 900,
       reset: true,
@@ -159,32 +54,36 @@ const animation = {
       origin: "bottom",
       distance: "90px"
     },
-   
-    zoomout: {
-      duration: 900,
-      opacity: 1,
-      scale: 1.5
-    },
-  
-   
-    Home:{
-        rotate: {
-            
-            y: 90,
-        
-            
-        },
-        opacity:0,
-        duration: 1000
+
+    Home: {
+      rotate: {
+        y: 90
+      },
+      opacity: 0,
+      duration: 1000
     }
   }
 };
 
 sr.reveal(".nav-bar", animation.allGadgets.navBar);
-sr.reveal(".nav-bar", animation.allGadgets.navBar);
 
 sr.reveal(".home-animation-h2", animation.desktop.Home_zoomin);
-sr.reveal(".home-animation-h2",{desktop:false});
-sr.reveal(".home-animation-p" , animation.allGadgets.top)
-sr.reveal(".home-animation-b" , animation.allGadgets.bottom)
-sr.reveal(".home" , animation.allGadgets.Home)
+
+sr.reveal(".home-animation-p", animation.allGadgets.top);
+sr.reveal(".home-animation-b", animation.allGadgets.bottom);
+sr.reveal(".home", animation.allGadgets.Home);
+sr.reveal(".latest-container", {
+  rotate: {
+    y: 90
+  },
+  opacity: 0,
+  duration: 900,delay: 500
+});
+sr.reveal(".news-left", {opacity: 0,scale: 0.5,delay: 500});
+sr.reveal(".news-right", {opacity: 0,scale: 0.5 ,delay: 500});
+sr.reveal(".card-01", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 300});
+sr.reveal(".card-02", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 360});
+sr.reveal(".card-03", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 420});
+sr.reveal(".card-04", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 480});
+sr.reveal(".card-05", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 540});
+sr.reveal(".card-06", {origin:'bottom', distance:'100px',opacity: 0,scale: .5, delay: 600});
